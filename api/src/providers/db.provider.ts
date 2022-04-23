12002e027e0c82f23694083655fcfeefab8dc866
Diego Sosa <dhsosa98@gmail.com>
2022-04-23 20:50:22 -0300

    Update Database Provider

--- api/src/providers/db.provider.ts
+++ api/src/providers/db.provider.ts
@@ -1,5 +1,6 @@
 import { Sequelize } from 'sequelize-typescript';
 import { Dialect } from 'sequelize/types';
+import { Directory } from 'src/entities/directory.entity';
 import { TodoItem } from 'src/entities/todoItem.entity';
 
 export const DatabaseProviders = [
@@ -7,14 +8,14 @@ export const DatabaseProviders = [
     provide: 'SEQUELIZE',
     useFactory: async () => {
       const sequelize = new Sequelize({
-        dialect: process.env.DB_DIALECT as Dialect || 'mysql',
+        dialect: (process.env.DB_DIALECT as Dialect) || 'mysql',
         host: process.env.DB_HOST || 'localhost',
         port: parseInt(process.env.DB_PORT || '3306'),
         username: process.env.DB_USERNAME || 'root',
         password: process.env.DB_PASSWORD || 'root',
         database: process.env.DB_NAME || 'ensolvers-challenge',
       });
-      sequelize.addModels([TodoItem]);
+      sequelize.addModels([TodoItem, Directory]);
       await sequelize.sync();
       return sequelize;
     },
