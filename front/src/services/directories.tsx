847ad46e3a1e5228ac5c4436a1a6e989f22a3801
Diego Sosa <dhsosa98@gmail.com>
2022-04-23 20:58:51 -0300

    Create Directory Service

---
+++ front/src/services/directories.tsx
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { ICreateDirectory } from "../interfaces/ICreateDirectory";
+
+const baseURL = "http://localhost:3001/directories";
+
+export const getDirectories = async () => {
+    return await axios.get(baseURL)
+}
+
+export const createDirectory = async (directory: ICreateDirectory) => {
+    return await axios.post(baseURL, directory)
+}
+
+export const deleteDirectory = async (id: number) => {
+    return await axios.delete(`${baseURL}/${id}`)
+}
+
+export const getDirectory = async (id: number) => {
+    return await axios.get(`${baseURL}/${id}`)
+}
