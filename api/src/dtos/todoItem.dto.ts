d7424bae67820001fc42b5793881cf9fb8befdfd
Diego Sosa <dhsosa98@gmail.com>
2022-04-23 20:49:14 -0300

    Update TodoItemDto

--- api/src/dtos/todoItem.dto.ts
+++ api/src/dtos/todoItem.dto.ts
@@ -1,9 +1,13 @@
-import { IsBoolean, IsString } from 'class-validator';
+import { IsBoolean, IsNumber, IsString, MinLength } from 'class-validator';
 
 export class TodoItemDto {
   @IsString()
+  @MinLength(3)
   description: string;
 
   @IsBoolean()
   selected: boolean;
+
+  @IsNumber()
+  directoryId: number;
 }
