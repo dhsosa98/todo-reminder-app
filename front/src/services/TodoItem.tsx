6d485771c0dd456c7ab61af02a731414a6cd1b1b
Diego Sosa <dhsosa98@gmail.com>
2022-04-23 20:47:22 -0300

    updated todoItem Service

--- front/src/services/TodoItem.tsx
+++ front/src/services/TodoItem.tsx
@@ -1,30 +1,25 @@
-import axios from 'axios'
-import { ICreateTodoItem } from '../interfaces/ICreateTodoItem'
-import { ITodoItem } from '../interfaces/ITodoItem'
+import axios from "axios";
+import { ICreateTodoItem } from "../interfaces/ICreateTodoItem";
+import { ITodoItem } from "../interfaces/ITodoItem";
 
-const baseURL = 'http://localhost:3001/todoitems'
+const baseURL = "http://localhost:3001/todoitems";
 
 export const getTodoList = async () => {
-    return await axios.get(`${baseURL}`)
-}
+  return await axios.get(`${baseURL}`);
+};
 
 export const deleteTodoItem = async (id: number) => {
-    try{
-    return await axios.delete(`${baseURL}/${id}`)
-    }
-    catch(err){
-        console.log({err})
-    }
-}
+  return await axios.delete(`${baseURL}/${id}`);
+};
 
 export const updateTodoItem = async (id: number, todoItem: ITodoItem) => {
-    return await axios.put(`${baseURL}/${id}`, todoItem)
-}
+  return await axios.put(`${baseURL}/${id}`, todoItem);
+};
 
 export const createTodoItem = async (todoItem: ICreateTodoItem) => {
-    return await axios.post(baseURL, todoItem)
-}
+    return await axios.post(baseURL, todoItem);
+};
 
 export const getTodoItem = async (id: number) => {
-    return await axios.get(`${baseURL}/${id}`)
-}
+  return await axios.get(`${baseURL}/${id}`);
+};
