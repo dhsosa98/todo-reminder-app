caaeb353ef7118d84404602cb8de8a332a072f6e
Diego Sosa <dhsosa98@gmail.com>
2022-04-23 15:58:19 -0300

    Create Interfaces

---
+++ front/src/interfaces/ICreateTodoItem.tsx
@@ -0,0 +1,4 @@
+export interface ICreateTodoItem{
+    description: string,
+    selected: boolean, 
+}
---
+++ front/src/interfaces/ITodoItem.tsx
@@ -0,0 +1,5 @@
+export interface ITodoItem{
+    id: number,
+    description: string,
+    selected: boolean, 
+}
