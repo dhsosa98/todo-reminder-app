82e477c75cfb564792b1719e6a132bda179b7cfe
Diego Sosa <dhsosa98@gmail.com>
2022-04-23 21:13:04 -0300

    Update config.js

--- api/src/database/config.js
+++ api/src/database/config.js
@@ -3,12 +3,12 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    dialect: process.env.DB_DIALECT,
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    dialect: process.env.DB_DIALECT || 'mysql',
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT) || 3306,
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || 'root',
+    database: process.env.DB_NAME || 'ensolvers-challenge',
   },
   test: {
     dialect: process.env.DB_DIALECT,
