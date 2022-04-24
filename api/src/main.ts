5b5035d6d809ca624c7e23ce11b8106aa2011c50
Diego Sosa <dhsosa98@gmail.com>
2022-04-24 00:42:10 -0300

    Update API_PORT

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,11 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+/* eslint-disable  */
+require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3001);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
