93f7b50b8d85a75ca76d99c211f5ee927e403c92
Diego Sosa <dhsosa98@gmail.com>
2022-04-24 00:42:39 -0300

    Create GlobalStyle Component

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import EditTodoItem from './components/EditTodoItem'
 import Directories from './routes/Directories'
 import Todo from './routes/Todo'
+import styled, {createGlobalStyle} from 'styled-components'
 
 function App() {
 
   return (
     <BrowserRouter>
+    <GlobalStyle/>
     <Routes>
       <Route path='/' element={<Directories/>} />
       <Route path='/:directoryId' element={<Todo/>} />
@@ -17,3 +19,16 @@ function App() {
 }
 
 export default App
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background-color: #f5f5f5;
+  }
+`
