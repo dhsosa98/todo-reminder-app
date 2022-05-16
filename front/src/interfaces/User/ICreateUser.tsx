3ba1bf686d6859d4dfb044f3ea5ff55fcdff0271
Diego Sosa <dhsosa98@gmail.com>
2022-05-16 11:36:33 -0300

    Create user Interfaces

---
+++ front/src/interfaces/User/ICreateUser.tsx
@@ -0,0 +1,6 @@
+export interface ICreateUser {
+    username: string;
+    name: string;
+    surname: string;
+    password: string;
+}
---
+++ front/src/interfaces/User/ILoginUser.tsx
@@ -0,0 +1,4 @@
+export interface ILoginUser {
+    username: string;
+    password: string;
+}
