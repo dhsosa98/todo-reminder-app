17085c5be1a7913bf93d7adeead90217e9b62fd5
Diego Sosa <dhsosa98@gmail.com>
2022-05-16 11:23:36 -0300

    added user to database provider

--- api/src/providers/db.provider.ts
+++ api/src/providers/db.provider.ts
@@ -2,6 +2,7 @@ import { Sequelize } from 'sequelize-typescript';
 import { Dialect } from 'sequelize/types';
 import { Directory } from 'src/entities/directory.entity';
 import { TodoItem } from 'src/entities/todoItem.entity';
+import { User } from 'src/entities/user.entity';
 
 export const DatabaseProviders = [
   {
@@ -15,7 +16,7 @@ export const DatabaseProviders = [
         password: process.env.DB_PASSWORD || 'root',
         database: process.env.DB_NAME || 'ensolvers-challenge',
       });
-      sequelize.addModels([TodoItem, Directory]);
+      sequelize.addModels([TodoItem, Directory, User]);
       await sequelize.sync();
       return sequelize;
     },
