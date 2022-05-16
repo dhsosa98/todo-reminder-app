4c2dc1d28cf05338d237261aa47d1969444d5a08
Diego Sosa <dhsosa98@gmail.com>
2022-05-16 11:31:48 -0300

    Add api route prefix

--- api/src/main.ts
+++ api/src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
