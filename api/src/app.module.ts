f6e720a60d994cdfc648b1189e05e0b685ebb645
Diego Sosa <dhsosa98@gmail.com>
2022-05-16 11:31:33 -0300

    Add authentication

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthController } from './controllers/auth.controller';
+import { JwtAuthGuard } from './guards/auth.guard';
+import { AuthModule } from './modules/auth.module';
 import { DirectoryModule } from './modules/directory.module';
 import { todoItemModule } from './modules/todoItem.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), todoItemModule, DirectoryModule],
+  imports: [
+    ConfigModule.forRoot(),
+    todoItemModule,
+    DirectoryModule,
+    AuthModule,
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
